41b35b65fddd42613b0f2ce9f7f61819bd8ee4f3
Joshua Chapman <john.chy99@gmali.com>
2019-06-03 19:52:58 +0200

    Visual studio

--- src/GestioneVettore.s
+++ src/GestioneVettore.s
@@ -10,10 +10,29 @@
 	.type main, @function
 
 // Data Section
-	.data
+    .data
 
-LUNGHEZZA_VETTORE:
-    .int 10
+    .global VETTORE
+VETTORE:
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+    .int 0
+
+STRINGA_INSERIMENTO:
+    .string "Inserimento dei 10 interi che compongono il vettore...\n"
+
+STRINGA_INSERIMENTO_ELEMENTO:
+    .string "Inserire l'intero in posizione %i: "
+
+STRINGS_FORMAT_I:
+    .string "%i"
 
 // Code Section
 	.text
@@ -21,8 +40,26 @@ LUNGHEZZA_VETTORE:
 main:
     pushq   %rbp
     movq    %rsp, %rbp
-
+    subq    $4, %rsp
+    leaq    STRINGA_INSERIMENTO(%rip), %rdi
+    callq   puts
+    movq    $3, -4(%rbp)
+main_insert:
+    movq    -4(%rbp), %rsi
+    incq    %rsi
+    leaq    STRINGA_INSERIMENTO_ELEMENTO(%rip), %rdi
+    xorq    %rax, %rax
+    callq   printf
+    leaq    VETTORE(%rip), %rax
+    movq    (%rbp), %rbx
+    leaq    (%rax, %rbx,4), %rsi
+    leaq    STRINGS_FORMAT_I(%rip), %rdi
+    xorq    %rax, %rax
+    callq   scanf
+    incq    (%rbp)
+    cmpq    $10, (%rbp)
+    jl      main_insert
     movq    %rbp, %rsp
     popq    %rbp
     xorq    %rax, %rax
-    ret
+    retq
