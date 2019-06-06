d0256f1a1f7fe65c031d1a02ce7dedfec39b6358
Joshua Chapman <john.chy99@gmali.com>
2019-06-06 11:03:27 +0200

    fatto forse

--- src/GestioneVettore.s
+++ src/GestioneVettore.s
@@ -2,9 +2,12 @@
     Punto di entrata dell'appicazione
 */
 
-    .type puts, @function
+// Funzioni esterne
+
     .type printf, @function
     .type scanf, @function
+
+// Funzioni interne
 
 	.global main
 	.type main, @function
@@ -15,8 +18,11 @@
 VETTORE:
     .int 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
+LUNGHEZZA_VETTORE:
+    .int 10
+
 STRINGA_INSERIMENTO:
-    .string "Inserimento dei 10 interi che compongono il vettore..."
+    .string "Inserimento dei %i interi che compongono il vettore...\n"
 
 STRINGA_INSERIMENTO_ELEMENTO:
     .string "Inserire l'intero in posizione %i: "
@@ -31,10 +37,23 @@ main:
     push    %rbp
     mov     %rsp, %rbp
     sub     $0x10, %rsp
-    lea     -0xc(%rbp), %rsi
+    mov     LUNGHEZZA_VETTORE(%rip), %rsi
+    lea     STRINGA_INSERIMENTO(%rip), %rdi
     xor     %rax, %rax
-    lea     STRINGS_FORMAT_I(%rip), %rdi
-    call    scanf
+    call    printf
+    movq     $0, -0xc(%rbp)
+    jmp     main_insert_loop_condition
+main_insert_loop:
+    mov     -0x8(%rbp), %rsi
+    add     $1, %rsi
+    lea     STRINGA_INSERIMENTO_ELEMENTO(%rip), %rdi
+    xor     %rax, %rax
+    call    printf
+    add     $1, -0xc(%rbp)
+main_insert_loop_condition:
+    mov     LUNGHEZZA_VETTORE(%rip), %rax
+    cmp     %rax, -0xc(%rbp)
+    jl      main_insert_loop
     mov     %rbp, %rsp
     pop     %rbp
     xor     %rax, %rax
