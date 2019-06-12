92d1ffc74f8a9a2f9725615dc5ed603319e9acc3
Davide <bleggi1094@gmail.com>
2019-06-12 11:31:03 +0200

    cerca valore iniziato

---
+++ src/functions/cercaValore.s
@@ -0,0 +1,15 @@
+/* cercaValore */
+    .global CercaValori
+    .type CercaValori @function
+
+    .data
+
+    .text
+
+CarcaValori: 
+    push    %rbp
+    mov     %rbp, %rsp
+    
+    mov     %rbp, %rsp
+    pop     %rbp
+
