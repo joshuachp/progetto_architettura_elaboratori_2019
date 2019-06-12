788f138700fdd9655345bfb6e9b49401b813bdba
luigi <luigi_sandoval99@ymail.com>
2019-06-12 15:42:47 +0200

    calcolaMax 1. stesura

--- src/functions/calcolaMax.s
+++ src/functions/calcolaMax.s
@@ -1,23 +1,37 @@
 /*
     File: calcolaMax.s
-    Descrizione: Punto di entrata dell'appicazione
+    Descrizione: Calcolo del massimo.
 */
 
     .file "calcolaMax.s"
 
-// Funzioni esterne
-
-    .type printf, @function
-    .type puts, @function
-
-// Data section
-    .data
-
-// Code section
+// Code section.
     .text
 
     .global calcolaMax
     .type calcolaMax @function
 
 calcolaMax:
-    
+    push    %rbp
+    mov     %rsp, %rbp
+    // Contatore ciclo.
+    xor     %rdx, %rdx 
+    // Settaggio massimo uguale vettore[0].
+    mov     VETTORE(%rip), %eax
+    lea     VETTORE(%rip), %rbx
+    jmp     calcoloMax_condizione
+calcolaMax_ciclo:
+    cmp     (%rbx, %rdx, 4), %eax
+    // Se massimo è maggiore del vettore, salto.
+    jg      calcolaMax_end_if
+    mov     (%rbx, %rdx, 4), %rax
+calcolaMax_end_if:
+    incl    %edx
+calcoloMax_condizione:
+    // Confronto contatore e lunghezza vettore.
+    cmp     LUNGHEZZA_VETTORE(%rip), %edx
+    jl      calcolaMax_ciclo
+    mov     %rbp, %rsp
+    pop     %rbp
+    ret
+ 
