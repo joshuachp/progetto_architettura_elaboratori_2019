f3c422850290e9c7299f07abc17456c93fb319eb
Joshua Chapman <john.chy99@gmali.com>
2019-06-21 15:23:32 +0200

    Finished stampa vettore

--- test/test.cpp
+++ test/test.cpp
@@ -4,17 +4,27 @@ extern "C" {
 #include "gestione_vettore.h"
 }
 
+void inizializa_VETTORE() {
+    for (int i = 0; i < LUNGHEZZA_VETTORE; i++) {
+        VETTORE[i] = i;
+    }
+}
+
 TEST(stampa_opzioni_success, stampa_opzioni) {
     ASSERT_NO_FATAL_FAILURE(stampaOpzioni());
 }
 
-/* TEST(stampa_vettore_success, stampa_vettore) {
+TEST(stampa_vettore_success, stampa_vettore) {
+    inizializa_VETTORE();
     ASSERT_NO_FATAL_FAILURE(stampaVettore(false));
-} */
+}
+
+TEST(stampa_vettore_inverso_success, stampa_vettore) {
+    inizializa_VETTORE();
+    ASSERT_NO_FATAL_FAILURE(stampaVettore(true));
+}
 
 TEST(numero_pari_incrementale, numero_pari) {
-    for (int i = 0; i < LUNGHEZZA_VETTORE; i++) {
-        VETTORE[i] = i;
-    }
+    inizializa_VETTORE();
     ASSERT_EQ(numeroPari(), LUNGHEZZA_VETTORE / 2);
 }
